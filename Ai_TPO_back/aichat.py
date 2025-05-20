5475692a82c298e99f059ffd0b3e86aee3434d00
JWORLD\cjw40 <cjw4032@gmail.com>
2025-05-20 21:20:44 +0900

    ai chat 기능 완료() main.py 엔드포인트 aichat.py API 설정, Gemini 모델 설정, 프롬프트 설정

--- Ai_TPO_back/aichat.py
+++ Ai_TPO_back/aichat.py
@@ -1,3 +1,4 @@
+# api 키 설정 Gemini 모델 설정 프롬프트 설정
 from pydantic import BaseModel
 import google.generativeai as genai
 import os
--- Ai_TPO_back/main.py
+++ Ai_TPO_back/main.py
@@ -1,3 +1,4 @@
+#엔드포인트 모음
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
